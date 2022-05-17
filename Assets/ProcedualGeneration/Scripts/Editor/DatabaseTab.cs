42c6402174fa8df66037b77af9f1bd9e22aa55ba
San_Rembak <61284264+SanRembak@users.noreply.github.com>
2022-05-17 22:30:29 +0300

    Update DatabaseTab.cs
    * Added list of items
    * Added possibility delete and load map data

--- Assets/ProcedualGeneration/Scripts/Editor/DatabaseTab.cs
+++ Assets/ProcedualGeneration/Scripts/Editor/DatabaseTab.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -13,14 +14,39 @@ public class DatabaseTab
     private string _databaseQuery;
     private string _databaseAnswer;
 
+    private Vector2 _scroll = Vector2.zero;
+
+    private List<MapData> _mapData = new List<MapData>();
+
     public void DisplayDatabaseTab()
     {
-        if(GUILayout.Button("Load"))
-        {
-            //DataTable decorationTable = Database.GetTable("SELECT * FROM DecorationSettings;");
-            //DecorationSettings decorationSettings = DecorationSettings.FromDataTable(decorationTable, 0);
+        DisplayHeader();
 
-            //GeneratorWindow.Instance.GeneratorTab.Init(new GlobalSettings(), new GraphicSettings(), new TerrainSettings(), decorationSettings);
+        _scroll = EditorGUILayout.BeginScrollView(_scroll, EditorStyles.helpBox, GUILayout.Height(500f));
+
+        foreach (var item in _mapData.ToArray())
+        {
+            DisplayItem(item);
+        }
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("Update data"))
+        {
+            RefreshMapData();
+        }
+    }
+
+    public void RefreshMapData()
+    {
+        _mapData = new List<MapData>();
+        DataRowCollection data = Database.GetTable("SELECT * FROM Map;").Rows;
+
+        foreach (DataRow row in data)
+        {
+            _mapData.Add(MapData.GetFromDataBase(row));
         }
     }
 
@@ -57,8 +83,76 @@ public class DatabaseTab
 
     }
 
-    public void DrawListElement(MapData data)
+    private void DisplayHeader()
     {
+        EditorGUILayout.Space();
+        GUIStyle headerLabelStyle = new GUIStyle(GUI.skin.label);
+        headerLabelStyle.fontSize = 14;
+        headerLabelStyle.fontStyle = FontStyle.Bold;
+        EditorGUILayout.LabelField("DATABASE", headerLabelStyle);
+        EditorGUILayout.Space();
+    }
 
+    public void DisplayItem(MapData mapData)
+    {
+        float labelWidth = EditorGUIUtility.labelWidth = 75f;
+        EditorGUILayout.IntField("ID: ", mapData.ID);
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUIUtility.wideMode = true;
+        EditorGUILayout.FloatField("Seed: ", mapData.Seed);
+
+        Vector2 size = new Vector2(mapData.Settings.GlobalSettings.Width, mapData.Settings.GlobalSettings.Height);
+        EditorGUILayout.Vector2Field("Size: ", size);
+        EditorGUILayout.FloatField("Fill amount: ", mapData.Settings.TerrainSettings.FillAmount);
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+
+        string terrainTileAssetPath = AssetDatabase.GUIDToAssetPath(mapData.GraphicSettings.TerrainTileGUID);
+        string backgroundTileAssetPath = AssetDatabase.GUIDToAssetPath(mapData.GraphicSettings.BackgroundTileGUID);
+        string decorationTileAssetPath = AssetDatabase.GUIDToAssetPath(mapData.GraphicSettings.DecorationTileGUID);
+
+        TileBase terrainTile = AssetDatabase.LoadAssetAtPath<TileBase>(terrainTileAssetPath);
+        TileBase backgroundTile = AssetDatabase.LoadAssetAtPath<TileBase>(backgroundTileAssetPath);
+        TileBase decorationTile = AssetDatabase.LoadAssetAtPath<TileBase>(decorationTileAssetPath);
+
+        EditorGUILayout.ObjectField("Terrain: ", terrainTile, typeof(TileBase), false);
+        EditorGUILayout.ObjectField("Background: ", backgroundTile, typeof(TileBase), false);
+        EditorGUILayout.ObjectField("Decoration: ", decorationTile, typeof(TileBase), false);
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+
+        if(GUILayout.Button("Delete"))
+        {
+            DeleteItem(mapData);
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            Load(mapData);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUIUtility.labelWidth = labelWidth;
+    }
+
+    public void DeleteItem(MapData mapData)
+    {
+        mapData.DeleteFromDatabase();
+        _mapData.Remove(mapData);
+    }
+
+    public void Load(MapData mapData)
+    {
+        if (GeneratorWindow.Instance == null) return;
+
+        GeneratorWindow.Instance.GeneratorTab.Init(mapData);
     }
 }
